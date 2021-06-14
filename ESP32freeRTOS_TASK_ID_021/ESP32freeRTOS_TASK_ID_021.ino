ace68d93ebf6206ea7add33ef4288ba97344af90
hamayan <hamayan@showa-ele.jp>
2021-06-14 11:54:48 +0900

    SP32でマルチタスクを行う為の、とりあえずここまで判った事 by freeRTOS ２１タスク目の例題。

---
+++ ESP32freeRTOS_TASK_ID_021/ESP32freeRTOS_TASK_ID_021.ino
@@ -0,0 +1,56 @@
+void setup()
+{
+  Serial.begin( 115200 );
+  Serial.println( "FreRTOS Test." );
+
+  /* configure take task1. */
+  xTaskCreatePinnedToCore(
+    lowerPriorityTask,  /* task name */
+    "",     /* task name string */
+    configMINIMAL_STACK_SIZE,   /* stack size */
+    NULL,   /* execute parameter */
+    2,      /* task priority : 0 to 24. 0 is lowest priority. */
+    NULL,   /* task handle pointer */
+    1       /* core ID */
+  );
+
+  /* configure take task2. */
+  xTaskCreatePinnedToCore(
+    upperPriorityTask,  /* task name */
+    "",     /* task name string */
+    configMINIMAL_STACK_SIZE,   /* stack size */
+    NULL,   /* execute parameter */
+    3,      /* task priority : 0 to 24. 0 is lowest priority. */
+    NULL,   /* task handle pointer */
+    1       /* core ID */
+  );
+  vTaskDelete( NULL );  /* delete loopTask. */
+}
+
+void loop()
+{
+}
+
+void lowerPriorityTask( void *execParam )
+{
+  int count = 1;
+  while( 1 )
+  {
+    vTaskSuspendAll();
+    Serial.print( "lowerPriorityTask count = " ); Serial.println( count++,DEC );
+    xTaskResumeAll();
+
+    vTaskDelay( pdMS_TO_TICKS( 1 ) );
+  }
+}
+
+void upperPriorityTask( void *execParam )
+{
+  int count = 1;
+  while( 1 )
+  {
+    Serial.print( "upperPriorityTask count = " ); Serial.println( count++,DEC );
+
+    vTaskDelay( pdMS_TO_TICKS( 10 ) );
+  }
+}
