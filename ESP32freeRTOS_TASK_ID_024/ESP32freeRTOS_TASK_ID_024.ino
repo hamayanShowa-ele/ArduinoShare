613d14725dda4d995463aa37575254c130990810
hamayan <hamayan@showa-ele.jp>
2021-06-14 12:57:36 +0900

    SP32でマルチタスクを行う為の、とりあえずここまで判った事 by freeRTOS ２４タスク目の例題。

---
+++ ESP32freeRTOS_TASK_ID_024/ESP32freeRTOS_TASK_ID_024.ino
@@ -0,0 +1,85 @@
+volatile SemaphoreHandle_t sema;
+
+void setup()
+{
+  Serial.begin( 115200 );
+  Serial.println( "FreRTOS Test." );
+
+  // Create binary semaphore
+  sema = xSemaphoreCreateBinary();
+  xSemaphoreGive( sema );  /* give one semaphore */
+
+  /* configure take task1. */
+  xTaskCreatePinnedToCore(
+    lowerPriorityTask,  /* task name */
+    "",     /* task name string */
+    configMINIMAL_STACK_SIZE,   /* stack size */
+    NULL,   /* execute parameter */
+    2,      /* task priority : 0 to 24. 0 is lowest priority. */
+    NULL,   /* task handle pointer */
+    1       /* core ID */
+  );
+
+  /* configure take task2. */
+  xTaskCreatePinnedToCore(
+    middlePriorityTask,  /* task name */
+    "",     /* task name string */
+    configMINIMAL_STACK_SIZE,   /* stack size */
+    NULL,   /* execute parameter */
+    3,      /* task priority : 0 to 24. 0 is lowest priority. */
+    NULL,   /* task handle pointer */
+    1       /* core ID */
+  );
+
+  /* configure take task3. */
+  xTaskCreatePinnedToCore(
+    upperPriorityTask,  /* task name */
+    "",     /* task name string */
+    configMINIMAL_STACK_SIZE,   /* stack size */
+    NULL,   /* execute parameter */
+    4,      /* task priority : 0 to 24. 0 is lowest priority. */
+    NULL,   /* task handle pointer */
+    1       /* core ID */
+  );
+  vTaskDelete( NULL );  /* delete loopTask. */
+}
+
+void loop()
+{
+}
+
+void lowerPriorityTask( void *execParam )
+{
+  while( 1 )
+  {
+    xSemaphoreTake( sema, portMAX_DELAY );
+    Serial.print( "lowerPriorityTask time = " ); Serial.println( millis(), DEC );
+    xSemaphoreGive( sema );
+
+    vTaskDelay( pdMS_TO_TICKS( 1 ) );
+  }
+}
+
+void middlePriorityTask( void *execParam )
+{
+  while( 1 )
+  {
+    vTaskDelay( pdMS_TO_TICKS( 20 ) );
+//    Serial.print( "middlePriorityTask time = " ); Serial.println( millis(), DEC );
+    unsigned long baseMillis = millis();
+    while( (millis() - baseMillis) < 100UL ) {}  /* never enter block state. */
+  }
+}
+
+void upperPriorityTask( void *execParam )
+{
+  int count = 1;
+  while( 1 )
+  {
+    xSemaphoreTake( sema, portMAX_DELAY );
+    Serial.print( "upperPriorityTask time = " ); Serial.println( millis() ,DEC );
+    xSemaphoreGive( sema );
+
+    vTaskDelay( pdMS_TO_TICKS( 10 ) );
+  }
+}
