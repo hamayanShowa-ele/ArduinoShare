8e2f98b52ee34ab26b16eefda4c60e46425b30cf
hamayan <hamayan@showa-ele.jp>
2021-07-08 14:13:12 +0900

    新規プロジェクトの追加。

--- F4_2102_Blynk_Blink/F4_2102_Blynk_Blink.ino
+++ F4_2102_Blynk_Blink/F4_2102_Blynk_Blink.ino
@@ -33,22 +33,65 @@
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-
-
 #include <SPI.h>
 #include <Ethernet2.h>
 #include <BlynkSimpleEthernet2.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "11111";
+
+// Remark "#include author.h" and write the Blynk token
+//  that you got yourself as follows instead.
+#include  "author.h"
+#ifndef  __AUTHORED__
+char auth[] = "type your blynk token.";  // token
+#endif  /* __AUTHORED__ */
+
+#define  ACT_LED    PB4
+#define  RED_LED    PB3
+
+#define  TXD6       PC6
+#define  RXD6       PC7
+#define  RTS6       PC8
+#define  TXD1       PA9
+#define  RXD1       PA10
+#define  USBDM      PA11
+#define  USBDP      PA12
+#define  USBON      PA15
+#define  SCL1       PB6
+#define  SDA1       PB7
 
 #define  SPI2_MOSI  PB15
 #define  SPI2_MISO  PB14
 #define  SPI2_SCK   PB13
 #define  W5500_CS   PB12
-#define  ACT_LED    PB4
+#define  W5500_RST  PA8
+#define  W5500_INT  PC9
 
+#define  DIO0       PB9
+#define  DIO1       PC13
+#define  DIO2       PC14
+#define  DIO3       PC15
+
+#define  IN00       PC0
+#define  IN01       PC1
+#define  IN02       PC2
+#define  IN03       PC3
+#define  IN04       PA0
+#define  IN05       PA1
+#define  IN06       PA2
+#define  IN07       PA3
+
+#define  OUT00      PA4
+#define  OUT01      PA5
+#define  OUT02      PA6
+#define  OUT03      PA7
+#define  OUT04      PC4
+#define  OUT05      PC5
+#define  OUT06      PB0
+#define  OUT07      PB1
+
+#define  LCD_RES    PB5
 
 void setup()
 {
@@ -57,6 +100,11 @@ void setup()
 
   pinMode( ACT_LED, OUTPUT );
   digitalWrite( ACT_LED, LOW );
+  pinMode( W5500_RST, OUTPUT );
+  digitalWrite( W5500_RST, LOW );
+  delay( 10UL );
+  digitalWrite( W5500_RST, HIGH );
+  delay( 500UL );
 
   // SPI connected with w5500 initialize.
   SPI.setMISO( SPI2_MISO );
@@ -64,11 +112,10 @@ void setup()
   SPI.setSCLK( SPI2_SCK );
   SPI.setSSEL( W5500_CS );
 
-  Blynk.begin(auth);
+  Blynk.begin( auth );  // or Blynk.begin( auth, uint8_t *mac );
   // You can also specify server:
   //Blynk.begin(auth, "blynk-cloud.com", 80);
   //Blynk.begin(auth, IPAddress(192,168,1,100), 8080);
-
 }
 
 void loop()
