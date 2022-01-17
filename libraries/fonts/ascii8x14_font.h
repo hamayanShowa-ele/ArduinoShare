931411d15b618b86e437bb320b99efa758abd2db
hamayan <43228503+hamayanShowa-ele@users.noreply.github.com>
2022-01-17 13:10:22 +0900

    Update ascii8x14_font.h

--- libraries/fonts/ascii8x14_font.h
+++ libraries/fonts/ascii8x14_font.h
@@ -1,8 +1,12 @@
-/****************************************************************************/
-/* ascii 8 width and 14 height font.                                        */
-/*                         Copyright (C) 2014 hamayan All Rights Reserved.  */
-/*                                                hamayan.contact@gmail.com */
-/****************************************************************************/
+/**
+  ******************************************************************************
+  * @file           : ascii8x14_font.h
+  * @brief          : ascii 8 width and 14 height font.
+    Copyright (c) 2022 Kazuaki Hamahara
+    Released under the MIT license
+    https://github.com/YukinobuKurata/YouTubeMagicBuyButton/blob/master/MIT-LICENSE.txt
+  ******************************************************************************
+  */
 #ifndef __ASCII8X14_FONT_H__
 #define __ASCII8X14_FONT_H__
 
@@ -12,8 +16,3 @@ extern const unsigned long ASCII8x14_PixHeight;
 extern const unsigned char ASCII8x14_GraphicBMPTable[];
 
 #endif  /* __ASCII8X14_FONT_H__ */
-
-/****************************************************************************/
-/*                         Copyright (C) 2014 hamayan All Rights Reserved.  */
-/*                                                hamayan.contact@gmail.com */
-/****************************************************************************/
