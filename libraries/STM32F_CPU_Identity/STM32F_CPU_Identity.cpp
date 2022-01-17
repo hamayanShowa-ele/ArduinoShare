4eaa7711aee554c7c5f4c7df8460537d6c64b23c
hamayan <43228503+hamayanShowa-ele@users.noreply.github.com>
2022-01-17 13:04:26 +0900

    Update STM32F_CPU_Identity.cpp

--- libraries/STM32F_CPU_Identity/STM32F_CPU_Identity.cpp
+++ libraries/STM32F_CPU_Identity/STM32F_CPU_Identity.cpp
@@ -1,6 +1,8 @@
 ﻿/*
   STM32Fxxx CPU Identity
-    Copyright (C) 2014 hamayan All Rights Reserved.
+  Copyright (c) 2022 Kazuaki Hamahara
+    Released under the MIT license
+    https://github.com/YukinobuKurata/YouTubeMagicBuyButton/blob/master/MIT-LICENSE.txt
 */
 #include  "STM32F_CPU_Identity.h"
 
