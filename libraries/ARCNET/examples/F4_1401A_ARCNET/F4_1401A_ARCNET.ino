f4bd15be1bca626c2e093e72dda15fb4b1281ee3
hamayan <hamayan@showa-ele.jp>
2022-07-28 15:03:41 +0900

    STM32F446ZET is now supported. The definitions of the pins were compiled into a header file.

--- libraries/ARCNET/examples/F4_1401A_ARCNET/F4_1401A_ARCNET.ino
+++ libraries/ARCNET/examples/F4_1401A_ARCNET/F4_1401A_ARCNET.ino
@@ -4,91 +4,33 @@
   This source code is released under the MIT license, but there are some different licenses.
   https://github.com/YukinobuKurata/YouTubeMagicBuyButton/blob/master/MIT-LICENSE.txt
 */
+/*
+  Arduino ide settings
+    U(S)ART support enabled(no generic Serial)
+    USB support CDC (generic Serial supersede U(S)ART)
+    USB speed (LOW/FULL speed)
+*/
 
 #define  BD_TYPE_1401
 //#define  BD_TYPE_1415
 
 /* Includes ------------------------------------------------------------------*/
-#include  <STM32_FSMC.h>
-#include  <ARCNET.h>
-#include  <DEBUG_UTIL.h>
+#include  <STM32F_CPU_Identity.h>  /* https://github.com/hamayanShowa-ele/ArduinoShare/tree/main/libraries/STM32F_CPU_Identity */
+#include  <STM32_FSMC.h>  /* https://github.com/hamayanShowa-ele/ArduinoShare/tree/main/libraries/STM32_FSMC */
+#include  <ARCNET.h>  /* https://github.com/hamayanShowa-ele/ArduinoShare/tree/main/libraries/ARCNET */
+#include  <DEBUG_UTIL.h>  /* https://github.com/hamayanShowa-ele/ArduinoShare/tree/main/libraries/DEBUG_UTIL */
 
 #if defined( BD_TYPE_1401 )
-  #include  <pca8574.h>
+  #include  <pca8574.h>  /* https://github.com/hamayanShowa-ele/ArduinoShare/tree/main/libraries/pca8574 */
+  #include  <bd1401a.h>  /* https://github.com/hamayanShowa-ele/ArduinoShare/blob/main/libraries/board_header/bd1401a.h */
 #elif defined( BD_TYPE_1415 )
+  #include  <bd1415a.h>  /* https://github.com/hamayanShowa-ele/ArduinoShare/blob/main/libraries/board_header/bd1415a.h */
 #endif /* defined( BD_TYPE_1401 ) */
 
 /* Private includes ----------------------------------------------------------*/
-
 /* Private typedef -----------------------------------------------------------*/
-
 /* Private define ------------------------------------------------------------*/
-
-#if defined( BD_TYPE_1401 )
-  #undef   LED_BUILTIN
-  #define  LED_RED      PF8
-  #define  LED_GREEN    PF10
-  #define  LED_BLUE     PF9
-  #define  ACTLED       PG11
-  #define  LED_BUILTIN  ACTLED
-  #define  USBON        PC0
-  #define  USBON_ACTIVE LOW
-  #define  TAMPER       PC13
-
-  #define  ETH_INT   PG6
-  #define  ARC_INT   PG7
-  #define  EXT_INT0  PG8
-  #define  EXT_INT1  PC9
-  #define  MRAM_CS   PD7
-  #define  ETH_CS    PG9
-  #define  ARC_CS    PG10
-  #define  EXT_CS    PG12
-  #define  IO_RST    PG13
-  #define  SDA1  PB9
-  #define  SCL1  PB8
-
-  #define  INT_SRAM_BASE_ADDRESS      0x60000000
-  #define  INT_ETHERNET_BASE_ADDRESS  0x64000000
-  #define  INT_ARCNET_BASE_ADDRESS    0x68000000
-  #define  EXT_BASE_ADR               0x6C000000
-  #define  INT_SRAM_SIZE              (524288UL * 2)  // SRAM
-  //#define  INT_SRAM_SIZE              (524288UL * 1)  // MRAM
-  #define  IC11_I2C_ADDRESS  0x20
-#elif defined( BD_TYPE_1415 )
-  #undef   LED_BUILTIN
-  #define  ACTLED       PB9
-  #define  LED_BUILTIN  ACTLED
-  #define  USBON        PA8
-  #define  USBON_ACTIVE HIGH
-  #define  ETH_INT   PC0
-  #define  ARC_INT   PC1
-  #define  EXT_INT0  PC4
-  #define  EXT_INT1  PC5
-  #define  EXT_INT2  PF6
-  #define  EXT_INT3  PF7
-  #define  EXT_INT4  PF8
-  #define  EXT_INT5  PF9
-  #define  EXT_INT6  PF10
-
-  #define  MRAM_CS   PD7
-  #define  ETH_CS    PG9
-  #define  ARC_CS    PG10
-  #define  EXT_CS    PG12
-  #define  ETH_RST   PC2
-  #define  ARC_RST   PC3
-  #define  IO_RST    ARC_RST
-  #define  SDA1  PB7
-  #define  SCL1  PB6
-
-  #define  INT_SRAM_BASE_ADDRESS      0x60000000
-  #define  INT_ETHERNET_BASE_ADDRESS  0x64000000
-  #define  INT_ARCNET_BASE_ADDRESS    0x68000000
-  #define  EXT_BASE_ADR               0x6C000000
-  #define  INT_SRAM_SIZE              (524288UL * 1)  // MRAM
-#endif /* defined( BD_TYPE_1401 ) */
-
 /* Private macro -------------------------------------------------------------*/
-
 /* Private variables ---------------------------------------------------------*/
 #if defined( BD_TYPE_1401 )
   TwoWire i2c1;
@@ -96,6 +38,7 @@
 #elif defined( BD_TYPE_1415 )
 #endif /* defined( BD_TYPE_1401 ) */
 
+STM32F_CPU_IDENTITY cpu_id;
 ARCNET arcnet( (volatile void *)INT_ARCNET_BASE_ADDRESS, ARC_INT, true, false );
 volatile uint8_t bdSW;
 uint32_t reportTime,transmitTime;
@@ -108,19 +51,20 @@ void setup()
 {
   pinMode( LED_BUILTIN, OUTPUT );
 
-  /* io reset start. */
-  pinMode( IO_RST, OUTPUT );
-  digitalWrite( IO_RST, LOW );
+  /* arcnet reset start. */
+  pinMode( ARC_RST, OUTPUT );  // IO_RST
+  digitalWrite( ARC_RST, LOW );  // IO_RST
   delay( 10 );
-  /* io reset end. */
-  digitalWrite( IO_RST, HIGH );
-  
+  /* arcnet reset end. */
+  digitalWrite( ARC_RST, HIGH );  // IO_RST
+
   /* bus configuration. */
 #if defined( BD_TYPE_1401 )
   fsmc_address( 19 );  // set address bus size
   fsmc_data( 16 );  // set data bus size.
-  fsmc_sram( FSMC_NORSRAM_BANK1, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 1,8,1,8, 0 );
-  fsmc_sram( FSMC_NORSRAM_BANK3, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 6,12,6,12, 0 );
+  fsmc_sram( FSMC_NORSRAM_BANK1, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 2,12,2,12, 0 );
+  fsmc_sram( FSMC_NORSRAM_BANK2, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 2,12,2,12, 0 );
+  fsmc_sram( FSMC_NORSRAM_BANK3, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 4,12,4,12, 0 );
   /* read cycle:209ns, read address setup time:80ns, read width:86ns */
   /* write cycle:210ns, write address setup time:87ns, write width:71ns */
   fsmc_sram( FSMC_NORSRAM_BANK4, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 8,8,8,8, 0 );
@@ -129,8 +73,8 @@ void setup()
 #elif defined( BD_TYPE_1415 )
   fsmc_address( 19 );  // set address bus size
   fsmc_data( 16 );  // set data bus size.
-  fsmc_sram( FSMC_NORSRAM_BANK1, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 1,4,1,4, 0 );
-//  fsmc_sram( FSMC_NORSRAM_BANK3, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 1,2,4,2, 0 );
+  fsmc_sram( FSMC_NORSRAM_BANK1, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 10,40,10,40, 0 );
+  fsmc_sram( FSMC_NORSRAM_BANK2, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 1,4,1,4, 0 );
   fsmc_sram( FSMC_NORSRAM_BANK3, FSMC_NORSRAM_MEM_BUS_WIDTH_16, 3,6,3,6, 0 );
   /* read cycle:209ns, read address setup time:80ns, read width:86ns */
   /* write cycle:210ns, write address setup time:87ns, write width:71ns */
@@ -139,17 +83,20 @@ void setup()
   /* write cycle:220ns, write address setup time:99ns, write width:46ns */
 #endif /* defined( BD_TYPE_1401 ) */
 
-  /*
-    Arduino ide settings
-      U(S)ART support enabled(no generic Serial)
-      USB support CDC (generic Serial supersede U(S)ART)
-      USB speed (LOW/FULL speed)
-  */
   pinMode( USBON, OUTPUT );
-  digitalWrite( USBON, USBON_ACTIVE );
+  digitalWrite( USBON, USB_POWER_ON_LEVEL );
   Serial.begin( 115200UL );
   while( !Serial ){ delay(20UL); }
-  Serial.println(); Serial.println( "hello serial output from USB." );
+  Serial.println();
+  Serial.println( "1401 or 1415 board testing program from USB Serial." );
+  Serial.println( "1401 or 1415 Arcnet demo." );
+  // cpu id and system clock.
+  String msg = "systic:"; msg += String( cpu_id.sysload() );
+  msg += " hclk:"; msg += String( cpu_id.hclk() );
+  msg += " pclk1:"; msg += String( cpu_id.pclk1() );
+  msg += " pclk2:"; msg += String( cpu_id.pclk2() );
+  msg += " sysclock:"; msg += String( cpu_id.sysclk() );
+  Serial.println( msg );
 
 #if defined( BD_TYPE_1401 )
   /* i2c initialize start. */
@@ -169,18 +116,18 @@ void setup()
 #endif /* defined( BD_TYPE_1401 ) */
   Serial.print( "BOARD SWITCH = " ); Serial.println( bdSW, DEC );
 
-  /* configuer arcnet. */
   /* Write access to the sram area and let the ARCNET controller determine the bus type. */
   uint32_t ret = memCheck( (uint8_t *)INT_SRAM_BASE_ADDRESS, INT_SRAM_SIZE );
   if( ret == 0 ) { Serial.println( "RAM is fine." ); }
   else { Serial.print( "There was a problem with the RAM at the following address. 0x" ); Serial.println( ret, HEX ); }
-//  dump( (const uint8_t *)INT_SRAM_BASE_ADDRESS, 256 );
+  //  dump( (const uint8_t *)INT_SRAM_BASE_ADDRESS, 256 );
 
   /* start arcnet controller. */
   Serial.println();
-  if( arcnet.begin( bdSW ) != 0 )
+  int result;
+  if( (result = arcnet.begin( bdSW )) != 0 )
   {
-    Serial.println( "ARCNET INITIALIZE ERROR." );
+    Serial.print( "ARCNET INITIALIZE ERROR code = " ); Serial.println( result, DEC );
     while( 1 ) delay( 1000 );
   }
   /* start arcnet interrupt. */
@@ -293,6 +240,7 @@ void cbARCNET_INT( void )
   *                        opensource.org/licenses/BSD-3-Clause
   */
 #if defined( BD_TYPE_1401 )
+#if defined( ARDUINO_GENERIC_F407ZETX )
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -331,6 +279,65 @@ void SystemClock_Config(void)
     user_Error_Handler();
   }
 }
+#elif defined( ARDUINO_GENERIC_F446ZETX )
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    user_Error_Handler();
+  }
+  /** Activate the Over-Drive mode
+  */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    user_Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    user_Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_CLK48;
+  PeriphClkInitStruct.PLLSAI.PLLSAIM = 8;
+  PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;
+  PeriphClkInitStruct.PLLSAI.PLLSAIQ = 2;
+  PeriphClkInitStruct.PLLSAI.PLLSAIP = RCC_PLLSAIP_DIV4;
+  PeriphClkInitStruct.PLLSAIDivQ = 1;
+  PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48CLKSOURCE_PLLSAIP;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    user_Error_Handler();
+  }
+}
+#endif  /* defined( ARDUINO_GENERIC_F407ZETX ) */
 #elif defined( BD_TYPE_1415 )
 void SystemClock_Config(void)
 {
