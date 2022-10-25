230c8865965dd8125b32611daa9f3375b521f355
hamayan <hamayan@showa-ele.jp>
2022-10-25 17:14:42 +0900

    Addition of master/slave bit definitions and example descriptions.

--- libraries/board_header/bd2113.h
+++ libraries/board_header/bd2113.h
@@ -6,11 +6,13 @@
 #ifndef  __BD2113_H__
 #define  __BD2113_H__
 
-#define  BD2113_ADC1_ADDRESS   0x20
-#define  BD2113_ADC2_ADDRESS   0x26
-#define  BD2113_PULSE_ADDRESS  0x24
-#define  BD2113_ADC_MASK  0x3FFF
+                                     /* Example of description in Class. */
+#define  BD2113_ADC1_ADDRESS   0x20  /* BD2113 adc1( (volatile uint16_t *)(EXT_BASE_ADR + BD2113_ADC1_ADDRESS) ); */
+#define  BD2113_ADC2_ADDRESS   0x26  /* BD2113 adc2( (volatile uint16_t *)(EXT_BASE_ADR + BD2113_ADC2_ADDRESS) ); */
+#define  BD2113_PULSE_ADDRESS  0x24  /* BD2113 pulse( (volatile uint16_t *)(EXT_BASE_ADR + BD2113_PULSE_ADDRESS) ); */
+#define  BD2113_ADC_MASK  0x3FFF  /* Example of reading a conversion value from ADC1. adc1.read() & BD2113_ADC_MASK */
 #define  BD2113_PPS_MASK  0x8000
+#define  BD2113_M_S_MASK  0x4000  /* Example of Master/Slave determination. (adc1.read() & BD2113_M_S_MASK) ? "MASTER" : "SLAVE" */
 
 class BD2113 // public hoge
 {
